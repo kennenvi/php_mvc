32d1c16cc034f02908879c28a2cb8a52e8afccf0
Aluno <aluno@ensino.umuarama.local>
2022-06-03 20:25:47 -0300

    Adding saving button to produto-alterar

--- produto-alterar.php
+++ produto-alterar.php
@@ -2,7 +2,22 @@
 
 require_once 'classes/Produto.php';
 
+// AÇÃO DO BOTÃO SAVAR
+// ATUALIZAÇÃO DO REGISTRO APÓS ALTERADO
+if (isset($_POST['salvar'])) {
+
+    $id = $_POST['id'];
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $status = $_POST['status'];
+
+    // Atualiza objeto
+    $produto = new Produto($id, $nome, $descricao, $status);
+}
+
 $id = $_GET['id'];
 $produto = new Produto($id);
 
 print_r($produto);
+
+// CRIAR INTERFACE QUE CARREGA O PRODUTO PARA ALTERARS
